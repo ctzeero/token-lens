a3836e3356bf2bcaa8b1cfedc152ca02d28929c8
Bien <>
2026-02-28 14:39:43 +0800

    Update formula

--- Formula/tokenlens.rb
+++ Formula/tokenlens.rb
@@ -1,9 +1,9 @@
 class Tokenlens < Formula
   desc "Check token usage for Cursor and other AI providers"
   homepage "https://github.com/ctzeero/tokenlens"
-  url "https://github.com/ctzeero/tokenlens/releases/download/1.0.0-beta/tokenlens"
-  sha256 "150e3865529501e19711aa51c75547ec0b697773a3ecc91dbd0ce257971cb1a8"
-  version "1.0.0-beta"
+  url "https://github.com/ctzeero/tokenlens/releases/download/1.0.1-beta/tokenlens"
+  sha256 "9778c9673a7ff04779d13211966e7af69cb296a100f14120e2c11053e5ec393a"
+  version "1.0.1-beta"
 
   def install
     bin.install "tokenlens" => "tlens"
